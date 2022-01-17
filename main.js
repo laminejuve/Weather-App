b079cfec2ea0f3e70c5f9a9df0664b71b6b84d6f
lamine <laminejuvee@gmail.com>
2022-01-17 22:08:01 +0100

    switch from celsius to fahrenheit

--- main.js
+++ main.js
@@ -11,24 +11,53 @@ const windSpeed = document.querySelector("#wind");
 const Hum = document.querySelector("#humidity");
 const input = document.querySelector("input");
 const cityLoc = document.querySelector(".location");
+const Cel_to_fah = document.querySelector("#checkbox");
 let city1 = "Moscow";
 
 
-cityLoc.addEventListener('click',function(e){
-  navigator.geolocation.getCurrentPosition((pos)=> {
+// switch between fahrenheit and celsius
+
+Cel_to_fah.addEventListener('change',function(e){
+  
+  if(e.target.checked){
+    const tmpF = temperature.innerHTML.split("°")[0];
+    temperature.innerHTML=Math.round((+tmpF*9/5) + 32)+"° F";
+    const feels_like_f = ((+(feels_like.innerHTML.split(":")[1].split("°")[0])*9/5) + 32);
+    feels_like.innerHTML = "Feels like : "+Math.round(feels_like_f)+"° F";
+  }else {
+    console.log("no")
+    const tmpC = document.querySelector("#temp").innerHTML.split("°")[0];
+    const feels_like_C = ((+(feels_like.innerHTML.split(":")[1].split("°")[0])-32) *5/9);
+    temperature.innerHTML=Math.round((+tmpC-32) *5/9)+"° C";
+    feels_like.innerHTML = "Feels like : "+Math.round(feels_like_C)+"° C";
+  }
+  
+  
+});
+
+// get location (latitude,longitude)
+const getCityByposition = function(){
+    navigator.geolocation.getCurrentPosition((pos)=> {
     const latitude = pos.coords.latitude;
     const longitude = pos.coords.longitude;
     getCityByCoords(latitude,longitude);
-    getWeather('annaba')
   },
   err => alert(err.message));
-})
+}
+getCityByposition();
+cityLoc.addEventListener('click',function(e){
+    getWeather(city1);
+  });
+
+// get weather for a given city when press the enter button
 
 document.addEventListener('keydown',function(e){
   if (e.key !== "Enter") return ;
   const city = e.target.value;
   getWeather(city);
 });
+
+// get weather for a city by its coords 
 
 const getCityByCoords = async function(latitude,longitude){
   try {
@@ -62,7 +91,6 @@ const renderData= function (data){
     cityName : data.name,
     timeZone : data.timezone,
   } 
-  console.log(weatherInfo);
   return weatherInfo;
 }
 
@@ -82,7 +110,6 @@ const insertData = function(data){
   sun_Set.innerHTML = "Sun Set : "+sunSet;
   //const today = new Date().toString().split("(")[0];
   const todayTime = calcTime(data.cityName,data.timeZone/3600);
-  console.log(todayTime);
   document.querySelector("h3").innerHTML = todayTime;
   const temp = Math.round(+(data.tempInfo.temp) - 273.15);
   temperature.innerHTML = temp+"° C";
@@ -91,7 +118,7 @@ const insertData = function(data){
   const wind = data.windSpeed;
   windSpeed.innerHTML ="Wind Speed : "+ wind+" Km/h";
   const weatherDes = data.weather.description;
-  description.innerHTML = weatherDes ;
+  description.innerHTML = weatherDes.toUpperCase() ;
   const weatherMain = data.weather.main;
   const weatherIcon = data.weather.icon;
   const humidity = data.tempInfo.humidity;
